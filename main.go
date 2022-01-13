39eb5b8001e3ee51f3da56e698fbbc8c6b203c2d
scott <scottsuk0306@kaist.ac.kr>
2022-01-13 23:07:33 +0900

    functions in Go

--- main.go
+++ main.go
@@ -1,6 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func lenAndUpper(name string) (length int, uppercase string) {
+	// defer command is evaluated when function call is finished.
+	defer fmt.Println("I'm done")
+	length = len(name)
+	uppercase = strings.ToUpper(name)
+	return
+}
 
 func main() {
 	fmt.Println("Hello World")
@@ -8,5 +19,7 @@ func main() {
 	name := "Scott"
 	// Variable Update
 	name = "Scott Suk"
+	length, _ := lenAndUpper(name)
+	fmt.Println(length)
 	fmt.Println(name)
 }
