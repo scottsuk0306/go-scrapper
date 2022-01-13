dae1177ea5db4996333f78fae2cadfefe6fe4c09
scott <scottsuk0306@kaist.ac.kr>
2022-01-13 23:22:05 +0900

    conditionals, switch in Go

--- main.go
+++ main.go
@@ -22,15 +22,50 @@ func superAdd(numbers ...int) int {
 	return total
 }
 
+func canIDrink(age int) bool {
+	if koreanAge := age + 2; koreanAge < 18 {
+		return false
+	}
+	return true
+}
+
+func canISmoke(age int) bool {
+	switch koreanAge := age + 2; koreanAge {
+	case 10:
+		return false
+	case 18:
+		return true
+	}
+	return false
+}
+
 func main() {
 	fmt.Println("Hello World")
 	// Variable Declaration
 	name := "Scott"
 	// Variable Update
 	name = "Scott Suk"
+
+	// Functions
 	length, _ := lenAndUpper(name)
 	fmt.Println(length)
 	fmt.Println(name)
-	sum_result = superAdd(1, 2, 3, 4, 5)
+
+	// For loop
+	sum_result := superAdd(1, 2, 3, 4, 5)
 	fmt.Println(sum_result)
+
+	// If Else
+	if canIDrink(16) {
+		fmt.Println("You can drink!")
+	} else {
+		fmt.Println("You cannot drink!")
+	}
+
+	// Switch
+	if canISmoke(10) {
+		fmt.Println("You can smoke!")
+	} else {
+		fmt.Println("You cannot smoke!")
+	}
 }
